4a17e0731678425dc3dc1e36d0ded903b534da7f
mad <bymanudominguez@gmail.com>
2023-11-27 18:21:05 -0300

    Initial commit

---
+++ src/main/java/com/utn/incidentreportingsystem/IncidentreportingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.utn.incidentreportingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentreportingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentreportingsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/incidentreportingsystem/IncidentreportingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.incidentreportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentreportingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
